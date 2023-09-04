508e1d381fdcf98f700ab453777621aa5f365c2b
Breezekid <calebbaraka79@gmail.com>
2023-09-04 18:29:05 +0300

    change

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <unstdi.h>
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success return 1
- * On error: -1 is returned and errno is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int_putchar(char c)
+int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
-
