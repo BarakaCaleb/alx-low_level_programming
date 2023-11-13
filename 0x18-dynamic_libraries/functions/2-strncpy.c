11375bdbf0c0f0b603fe653990fb069481c716bd
Breezekid <calebbaraka79@gmail.com>
2023-11-14 00:17:14 +0300

    1-create_dynamic

---
+++ 0x18-dynamic_libraries/functions/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - to copy a string
+ * @dest: the input value
+ * @src: the input value
+ * @n: the input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/functions/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes for the absolute value of an integer
+ * @n: the int to be check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
