81de18dcc3a0d54734ca5430a794c1e4c5f06f94
Breezekid <calebbaraka79@gmail.com>
2024-02-22 15:38:28 +0300

    changes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,11 @@
-!#/usr/bin/python3
-"""No module is to be imported"""
+#!/usr/bin/python3
+"""No module imported"""
+
 
 def island_perimeter(grid):
-    """This function returns the perimeter of an island."""
+    """This function gets the perimeter of an island"""
     perimeter = 0
     for list in grid:
         for i in list:
             perimeter = perimeter + i
     return (perimeter + 1) * 2
-
